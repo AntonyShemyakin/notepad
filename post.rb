5495390f0315f9880aef12e27496d01f76736f68
AntonS <shem86a@yandex.ru>
2020-11-24 15:42:19 +0500

    Notepad v.1, working

--- post.rb
+++ post.rb
@@ -1,32 +1,39 @@
 class Post
 
-  def initialize
-    @created_at = Time.now
-    @text = nil
+  def self.post_types
+    [Memo, Link, Task]
   end
-
-  def read_from_console
-    # todo
+  def self.create(type_index)
+    return post_types[type_index].new
   end
-
-  def to_strings
-    # todo
-  end
-
-  def save
-    file = File.new(file_path, "w:UTF-8")
-
-    for item in to_strings do
-      file.puts(item)
+    def initialize
+      @created_at = Time.now
+      @text = nil
     end
 
-    file.close
-  end
-  def file_path
-    current_path = File.dirname(__FILE__)
+    def read_from_console
+      # todo
+    end
 
-    file_name = @created_at.strftime("#{self.class.name} _%Y-%m-%d_%H-%M-%S.txt")
+    def to_strings
+      # todo
+    end
 
-    return current_path + "/" + file_name
+    def save
+      file = File.new(file_path, "w:UTF-8")
+
+      for item in to_strings do
+        file.puts(item)
+      end
+
+      file.close
+    end
+
+    def file_path
+      current_path = File.dirname(__FILE__)
+
+      file_name = @created_at.strftime("#{self.class.name} _%Y-%m-%d_%H-%M-%S.txt")
+
+      return current_path + "/" + file_name
+    end
   end
-end
